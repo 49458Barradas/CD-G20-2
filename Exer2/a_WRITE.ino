60c929fcb1d2cc86aa1c87cd82b061ab96097272
49458Barradas <A49458@alunos.isel.pt>
2023-06-18 18:50:20 +0100

    2a feito

--- Exer2/a_WRITE.ino
+++ Exer2/a_WRITE.ino
@@ -1,55 +1,28 @@
 // VARIAVEIS DE INPUT DA FUNÇÃO DE PROGRESSAO GEOMETRICA
-int n = 6; // número de termos
-int u = 2;  // primeiro termo
-int r = 3;  // razão
+int n = 6;         // número de termos
+int u = 2;         // primeiro termo
+int r = 3;         // razão
+
+int* termos;       // Ponteiro para armazenar os termos calculados
 
 void setup() {
   Serial.begin(9600);
+  termos = new int[n];
+  checksums = new int[n];
 }
 
 void loop() {
-  // Função de progressão geométrica
+  // Calcular os termos (32 bits)
   for (int i = 0; i < n; i++) {
     int termo = u;
     for (int j = 1; j <= i; j++) {
       termo *= r;
     }
-    enviarBits(termo);
-    delay(500);
+    termos[i] = termo;
+    Serial.print(termos[i]);
+    Serial.print(",");
   }
+  Serial.print("#");
 
-  // Envia o Fletcher checksum
-  uint32_t checksum = calcularChecksum();
-  enviarBits(checksum); // Envia o checksum em bits para o PC
-  delay(500);          // Aguarda 500 milissegundos antes de enviar o fim da sequência
-  enviarFimSequencia();  // Envia o fim da sequência
-  delay(500);
-}
-
-void enviarBits(uint32_t numero) {
-  for (int i = 31; i >= 0; i--) {
-    int bit = (numero >> i) & 1;
-    Serial.print(bit);
-  }
-  Serial.println(); // Imprime uma nova linha após enviar todos os bits
-}
-
-uint32_t calcularChecksum() {
-  uint32_t sum1 = 0xFFFF;
-  uint32_t sum2 = 0xFFFF;
-
-  for (int i = 0; i < n; i++) {
-    int termo = u;
-    for (int j = 1; j <= i; j++) {
-      termo *= r;
-    }
-    sum1 = (sum1 + termo) % 65535;
-    sum2 = (sum2 + sum1) % 65535;
-  }
-
-  return (sum2 << 16) | sum1;
-}
-
-void enviarFimSequencia() {
-  Serial.println("#"); // Caractere especial para indicar o fim da sequência
+  delay(1000);
 }
---
+++ Exer2/bRead.py
@@ -0,0 +1,29 @@
+import serial
+
+###############################################################
+# COM TÉCNICA FLETCHER SUM
+###############################################################
+
+porta_serial = "COM3"
+ser = serial.Serial(porta_serial, 9600, timeout=1)
+
+def main():
+    stop = False
+    while not stop:
+        data = ""
+        end = False
+        while not end:
+            temp = ser.read().decode()
+            if temp == "#":
+                break
+            if temp != ",":
+                data+=temp
+            if temp == ",":
+                end = True
+        print(data)
+        if temp == "#":
+            break
+
+
+if __name__ == '__main__':
+    main()
